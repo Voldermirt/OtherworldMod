ef38a4bdd2c40f2e7bf6f47737ff0d5566e48950
Voldermirt <emars.nighting@gmail.com>
2023-01-02 11:53:18 -0800

    Initial commit

---
+++ src/main/java/net/voldermirt/otherworld/OtherworldMod.java
@@ -0,0 +1,14 @@
+package net.voldermirt.otherworld;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OtherworldMod implements ModInitializer {
+	public static final String MOD_ID = "otherworld";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/voldermirt/otherworld/OtherworldModClient.java
@@ -0,0 +1,11 @@
+package net.voldermirt.otherworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OtherworldModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
